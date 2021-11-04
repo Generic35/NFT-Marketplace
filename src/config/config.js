7dd62eb86f5b5543fea42d7617aa28d221ae2985
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-04 17:46:34 -0400

    feat: Add ability to query an existing contract

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
-import {config} from "@onflow/fcl"
+import { config } from "@onflow/fcl";
 
 config({
-	"accessNode.api": process.env.REACT_APP_ACCESS_NODE,
-	"discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY
+  "accessNode.api": process.env.REACT_APP_ACCESS_NODE,
+  "discovery.wallet": process.env.REACT_APP_WALLET_DISCOVERY,
+  "0xDappy": process.env.REACT_APP_DAPPY_CONTRACT,
 });
