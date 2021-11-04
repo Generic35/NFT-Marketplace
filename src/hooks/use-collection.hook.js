451f4bdeaf5aea37f9212988339f73effcdda41c
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-04 18:28:03 -0400

    feat: Add ability to check if a collection exists on a contract at

--- src/hooks/use-collection.hook.js
+++ src/hooks/use-collection.hook.js
@@ -1,8 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {query} from '@onflow/fcl'
+import { CHECK_COLLECTION } from '../flow/check-collection.script'
 
-export default function useCollection() {
-  const [loading] = useState(true)
+export default function useCollection(user) {
+  const [loading, setLoading] = useState(true)
   const [collection, setCollection] = useState(false)
+
+	useEffect(()=>{
+		if(!user?.addr) return
+		const checkCollection = async () => {
+			try {
+				let res = await query({
+					cadence: CHECK_COLLECTION,
+					args: (arg, t) => [arg(user?.addr, t.Address)]
+
+				})
+				setCollection(res);
+				console.log(res)
+				setLoading(false)
+			} catch (err){
+				console.log(err)
+				setLoading(false)
+			}
+		}
+		checkCollection()
+	},[user?.addr])
 
   const createCollection = async () => {
     setCollection(true)
